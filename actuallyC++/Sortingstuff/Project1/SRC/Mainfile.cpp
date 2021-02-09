1b48d70252ca6e38bf4c1b09beaaaa4e7a6416a9
Vikas Yadav <32956678+talktovik@users.noreply.github.com>
2021-02-09 11:15:29 +0530

    IDK

--- actuallyC++/Sortingstuff/Project1/SRC/Mainfile.cpp
+++ actuallyC++/Sortingstuff/Project1/SRC/Mainfile.cpp
@@ -1,8 +1,30 @@
 #include"Header.h"
 int main() {
 	std::cout << "START" << std::endl;
-	int arr[] = { 6,3,5,3,9};
+	//int arr[] = { 6,3,5,3,9};
 	//Bubble_sort(arr, 5);// This is passing the array with the starting address. The function decleration knows that fun() need integer value.
-	Insertion_sort(arr, 5);
-
+	//Insertion_sort(arr, 5);
+	//int vik = 1;
+	//int sik = 2;
+	//int y;
+	//y = ++vik == sik-- ? 2 : 4;
+	//printf("%d\n", y);    // ?? 
+	//printf("Value of vik var is : %d", vik); //2
+	//printf("\n ");
+	//printf("value of sik var is : %d", sik);//1
+	//Type casting 
+	//int a = 23;
+	//float stm;
+	//double  var = 12.09;
+	//stm = (double)a + var;
+	//std::cout <<stm;
+	//printf(" ");
+	//std::cout << typeid(a).name() << std::endl;
+	/*int con;
+	std::cout << sizeof(con) << std::endl;
+	con = 4294967295;
+	std::cout << con;*/
+	std::uint32_t vik;
+	printf(typeid(vik).name());
+	printf(sizeof(vik));
 }
--- actuallyC++/Sortingstuff/Project1/SRC/sorting_techniques.cpp
+++ actuallyC++/Sortingstuff/Project1/SRC/sorting_techniques.cpp
@@ -6,7 +6,6 @@ void printing(int A[], int n) {
 	}
 }
 
-
 void swap(int* a, int* b) {
 	int temp;
 	//*a = temp;
@@ -58,11 +57,45 @@ void selectionsort(int a[], int n) {
 	for (int i = 0; i < n; i++) {
 		ptr = i;
 		srcptr = i;
-		if a[ptr]
+		if (a[ptr] < a[srcptr]){
+			swap(&a[ptr], &a[srcptr]);
+			//ptr
+		}
 	}
 
 }
 
 
+//void testing() {
+//	bool beginsAtSide(const _FaceGrid & sideGrid1,
+//		const _FaceGrid & sideGrid2,
+//		SMESH_ProxyMesh::Ptr proxymesh)
+//	{
+//		const TNodeColumn& col0 = sideGrid2._u2nodesMap.begin()->second;
+//		const TNodeColumn& col1 = sideGrid2._u2nodesMap.rbegin()->second;
+//		const SMDS_MeshNode* n00 = col0.front();
+//		const SMDS_MeshNode* n01 = col0.back();
+//		const SMDS_MeshNode* n10 = col1.front();
+//		const SMDS_MeshNode* n11 = col1.back();
+//		const SMDS_MeshNode* n = (sideGrid1._u2nodesMap.begin()->second)[0];
+//		if (proxymesh)
+//		{
+//			n00 = proxymesh->GetProxyNode(n00);
+//			n10 = proxymesh->GetProxyNode(n10);
+//			n01 = proxymesh->GetProxyNode(n01);
+//			n11 = proxymesh->GetProxyNode(n11);
+//			n = proxymesh->GetProxyNode(n);
+//		}
+//		return (n == n00 || n == n01 || n == n10 || n == n11);
+//	}
+//}
+//
+//
+//
+//}
+//
 
-
+//
+//using namespace vik;
+//
+//
