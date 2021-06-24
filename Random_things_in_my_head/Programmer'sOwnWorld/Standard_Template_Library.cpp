bdf054aa248c0be5072f8286cc3df33129d68699
Vikas Yadav <32956678+talktovik@users.noreply.github.com>
2021-06-24 15:38:02 +0530

    working on brushing my stls

---
+++ Random_things_in_my_head/Programmer'sOwnWorld/Standard_Template_Library.cpp
@@ -0,0 +1,21 @@
+#include "Standard_Template_Library.h"
+
+void Standard_Template_Library::DataStructures() 
+{
+			//Containers
+
+	//arrays
+	array<int,10> values; //create the array of size 10
+
+	//Vectors
+	vector<int> values;// Create the size now you can push_back the elements anytime actually.
+		//-----------------> here we don't have to worry about the size to give.
+	
+	//Deque
+	deque<int> vlaue_of_deques; //https://www.cplusplus.com/reference/deque/deque/
+
+	//Forward_list
+	forward_list<int> values;
+	
+
+}
---
+++ Random_things_in_my_head/Programmer'sOwnWorld/Standard_Template_Library.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<array>
+#include<vector>
+#include<deque>
+#include<forward_list>
+
+
+
+using namespace std;
+class Standard_Template_Library
+{
+	void DataStructures();
+	void Algorithms();
+};
+
