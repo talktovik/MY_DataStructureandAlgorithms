91cb1618a81c71b39b7b81a8c2c8b17874cacd50
Vikas Yadav <32956678+talktovik@users.noreply.github.com>
2021-08-06 12:16:49 +0530

    Create randint.cpp

---
+++ randint.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdC++.h>
+#inlcude<time.h>
+
+using namespace std;
+
+int selectRandom(int x)
+{
+  static int result;
+  static int count = 0;
+  count++;
+  if(count == 1)result = x;
+  else 
+  {
+    //rand() is predefined function in the header <bits/stdC++>, actually its in the header stdlib.h
+    int i = rand()%count;
+    if(i == count -1) result = x;
+  }
+  return result;
+}
+
+int main()
+{
+  int stream[] = {1,2,3,4}; //you can you the file I/O here as well
+  int n = sizeof(stream)/sizeof(stream[0]); //count the Number of elements in the container[Array here! ]
+  
+  srand(time(NULL));
+  for(int i = 0 i< n ;i++)
+    std::cout<<"here we get the value  "<< selectRandom(stream[i])<<std::endl;
+  return 1;
+}
